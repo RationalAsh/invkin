2bc78d52f614de5e4faf46e1e5c081e414158892
RationalAsh <ashwinnarayan1994@gmail.com>
2014-07-15 00:43:40 +0530

    Update arm_master.ino

--- arm_master.ino
+++ arm_master.ino
@@ -33,7 +33,17 @@ void setup() {
   m1.begin(9600);
   m2.begin(9600);
   m3.begin(9600);
-  m4.begin(9600);  
+  m4.begin(9600);
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
 }
 
 void loop() {
